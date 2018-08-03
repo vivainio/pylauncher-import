9d93d18e2aeafd163a81c115d14963b3304a58d0
Ville M. Vainio <ville.vainio@basware.com>
2018-08-03 21:37:11 +0300

    import from https://bitbucket.org/vinay.sajip/pylauncher

---
+++ buildmsi.py
@@ -0,0 +1,24 @@
+import getpass
+import os
+import sys
+
+VER = '1.0.1.7'
+VERSION = 'Version=%s' % VER
+MANUFACTURER = 'Manufacturer=Vinay Sajip'
+X86 = 'Platform=x86'
+X64 = 'Platform=x64'
+TOWIN = 'ToWindows'
+
+def main():
+    signpwd = getpass.getpass('Password for signing:')
+    import builddoc
+    builddoc.main()
+    os.environ['SIGNPWD'] = signpwd
+    import makemsi
+    makemsi.main(['-o', 'launchwin-%s' % VER, X86, VERSION, MANUFACTURER, TOWIN, 'launcher'])
+    makemsi.main(['-o', 'launcher-%s' % VER, X86, VERSION, MANUFACTURER, 'launcher'])
+    makemsi.main(['-o', 'launchwin-%s' % VER, X64, VERSION, MANUFACTURER, TOWIN, 'launcher'])
+    makemsi.main(['-o', 'launcher-%s' % VER, X64, VERSION, MANUFACTURER, 'launcher'])
+
+if __name__ == '__main__':
+    sys.exit(main())
